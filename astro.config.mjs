dfb670a97cde41aab36467cfcb1ee3fae514b12f
leochiu <leochiu.life@gmail.com>
2024-12-21 12:32:46 +0800

    chore: configure astro react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 import vercelServerless from "@astrojs/vercel/serverless";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercelServerless({
@@ -10,4 +12,6 @@ export default defineConfig({
       enabled: true,
     },
   }),
+
+  integrations: [react()],
 });
