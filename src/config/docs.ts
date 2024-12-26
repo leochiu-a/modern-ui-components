a670de9846c9795122ba7062934353acb1be4ec5
leochiu <leochiu.life@gmail.com>
2024-12-26 16:13:35 +0800

    feat: add main-mav

---
+++ src/config/docs.ts
@@ -0,0 +1,67 @@
+export const docsConfig = {
+  mainNav: [
+    {
+      title: "Components",
+      href: "/components",
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Getting Started",
+      items: [
+        {
+          title: "Introduction",
+          href: "/docs",
+          items: [],
+        },
+        {
+          title: "Installation",
+          href: "/docs/installation",
+          items: [
+            {
+              title: "Next.js",
+              href: `/docs/installation/next`,
+              items: [],
+            },
+            {
+              title: "Vite",
+              href: `/docs/installation/vite`,
+              items: [],
+            },
+            {
+              title: "Remix",
+              href: `/docs/installation/remix`,
+              items: [],
+            },
+            {
+              title: "Astro",
+              href: `/docs/installation/astro`,
+              items: [],
+            },
+            {
+              title: "Laravel",
+              href: `/docs/installation/laravel`,
+              items: [],
+            },
+            {
+              title: "Gatsby",
+              href: `/docs/installation/gatsby`,
+              items: [],
+            },
+            {
+              title: "Manual",
+              href: `/docs/installation/manual`,
+              items: [],
+              label: "",
+            },
+          ],
+        },
+        {
+          title: "CLI",
+          href: "/docs/cli",
+          items: [],
+        },
+      ],
+    },
+  ],
+};
---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "Component Library",
+  url: "",
+  ogImage: "",
+  description:
+    "Beautifully designed landing page components built with React & Tailwind CSS.",
+  links: {
+    twitter: "",
+    discord: "",
+    github: "",
+    instagram: "",
+  },
+  keywords: ["React", "Tailwind CSS", "Framer Motion", "Components"],
+};
