a7f58c34dda9d72e25724b68be9c0738f285d469
leochiu <leochiu.life@gmail.com>
2024-12-27 21:38:20 +0800

    feat: integrate content collection and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@
 import { defineConfig } from "astro/config";
 
 import vercelServerless from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
-
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -20,5 +19,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    mdx(),
   ],
 });
--- src/config/docs.ts
+++ src/config/docs.ts
@@ -2,7 +2,7 @@ export const docsConfig = {
   mainNav: [
     {
       title: "Components",
-      href: "/components",
+      href: "/components/button",
     },
   ],
   sidebarNav: [
@@ -12,54 +12,27 @@ export const docsConfig = {
         {
           title: "Introduction",
           href: "/docs",
-          items: [],
         },
         {
           title: "Installation",
           href: "/docs/installation",
-          items: [
-            {
-              title: "Next.js",
-              href: `/docs/installation/next`,
-              items: [],
-            },
-            {
-              title: "Vite",
-              href: `/docs/installation/vite`,
-              items: [],
-            },
-            {
-              title: "Remix",
-              href: `/docs/installation/remix`,
-              items: [],
-            },
-            {
-              title: "Astro",
-              href: `/docs/installation/astro`,
-              items: [],
-            },
-            {
-              title: "Laravel",
-              href: `/docs/installation/laravel`,
-              items: [],
-            },
-            {
-              title: "Gatsby",
-              href: `/docs/installation/gatsby`,
-              items: [],
-            },
-            {
-              title: "Manual",
-              href: `/docs/installation/manual`,
-              items: [],
-              label: "",
-            },
-          ],
         },
         {
           title: "CLI",
           href: "/docs/cli",
-          items: [],
+        },
+      ],
+    },
+    {
+      title: "Components",
+      items: [
+        {
+          title: "Button",
+          href: "/components/button",
+        },
+        {
+          title: "Card",
+          href: "/components/card",
         },
       ],
     },
