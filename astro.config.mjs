f1aa437a0a58c2c36634fcb34296dfffad065e28
leochiu <leochiu.life@gmail.com>
2025-02-15 20:37:11 +0800

    chore: update astro.config to adopt new vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercelServerless from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercelServerless({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-
+  output: "server",
+  adapter: vercel(),
   integrations: [vue(), react(), mdx({})],
 });
